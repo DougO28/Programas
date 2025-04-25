bec309b29fa7e6ec55291f5d1ba4462ad644a0fe
Douglas Ortega <135684498+DougO28@users.noreply.github.com>
2025-04-25 17:53:48 -0600

    Update Main.py
    Modificaciones en dependencias

--- Cubo de Rubik/Main.py
+++ Cubo de Rubik/Main.py
@@ -1,13 +1,13 @@
 import pygame
-from config import *
-from cubo import CuboRubik
+from Config import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, COLORES
+from Cubo import CuboRubik
 
 pygame.init()
 pantalla = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Cubo de Rubik")
 reloj = pygame.time.Clock()
 
-cubo = CuboRubik()
+Cubo = CuboRubik()
 
 def dibujar_cubo():
     tam = 30
@@ -17,7 +17,7 @@ def dibujar_cubo():
         x0 = offset_x + (i % 4) * tam * 3 if cara != 'U' and cara != 'D' else offset_x + tam * 3
         y0 = offset_y if cara == 'U' else offset_y + (1 if cara in ['L','F','R','B'] else 4) * tam
         for j in range(9):
-            color = COLORES[cubo.caras[cara][j]]
+            color = COLORES[Cubo.caras[cara][j]]
             x = x0 + (j % 3) * tam
             y = y0 + (j // 3) * tam
             pygame.draw.rect(pantalla, color, (x, y, tam, tam))
@@ -30,7 +30,7 @@ while ejecutando:
             ejecutando = False
         if evento.type == pygame.KEYDOWN:
             if evento.key == pygame.K_u:
-                cubo.mover_U()
+                Cubo.mover_U()
 
     pantalla.fill((30, 30, 30))
     dibujar_cubo()
